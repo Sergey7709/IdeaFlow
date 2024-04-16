52a3f9f0664fb433d3eae7923a511ddec5756634
Sergey Koshelev <shivacharya2019@gmail.com>
2024-04-16 21:54:48 +0300

    feat: add .env Variables for API-KEY and baseURL

--- src/common/api/common.api.ts
+++ src/common/api/common.api.ts
@@ -3,10 +3,10 @@ import axios from "axios";
 const settings = {
   withCredentials: true,
   headers: {
-    "API-KEY": "b333b582-9a3b-4255-a036-8f36df44f2db",
+   "API-KEY": process.env.REACT_APP_API_KEY,
   },
 };
 export const instance = axios.create({
-  baseURL: "https://social-network.samuraijs.com/api/1.1/",
+  baseURL: process.env.REACT_APP_API_BASE_URL,
   ...settings,
 });
