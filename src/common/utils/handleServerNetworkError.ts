82b69e162cd6d209d75deab68d8aa1a64a900f39
Sergey Koshelev <shivacharya2019@gmail.com>
2024-04-16 18:55:45 +0300

    refactor: enhance error handling in handleServerNetworkError by integrating BaseResponseType

--- src/common/utils/handleServerNetworkError.ts
+++ src/common/utils/handleServerNetworkError.ts
@@ -1,18 +1,19 @@
 import { appActions } from "../../app/app-reducer";
 import { AppDispatch } from "../../app/store";
 import axios from "axios";
+import {BaseResponseType} from "../types";
+
 
 export const handleServerNetworkError = (err: unknown, dispatch: AppDispatch): void => {
   let errorMessage = "Some error occurred";
 
   if (axios.isAxiosError(err)) {
-    errorMessage = err.response?.data?.message || err?.message || errorMessage;
+    errorMessage = err.response?.data?.message || err?.message ||  errorMessage;
   } else if (err instanceof Error) {
     errorMessage = `Native error: ${err.message}`;
   } else {
-    errorMessage = JSON.stringify(err);
-  }
-
+    errorMessage = (err as BaseResponseType)?.messages[0]  || JSON.stringify(err);
+    }
   dispatch(appActions.setAppError({ error: errorMessage }));
   dispatch(appActions.setAppStatus({ status: "failed" }));
 };
